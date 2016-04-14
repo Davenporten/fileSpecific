8ffeed78015752201a26082f894d1d2be3151e22
Davenporten <Davenporten@gmail.com>
2016-04-14 11:25:08 -0600

    Update chets.sh

--- chets.sh
+++ chets.sh
@@ -54,3 +54,30 @@ do
       fi
    done;
 done
+
+#for i in $1/*;
+#do
+#   for j in $2/*;
+#   do
+#      FILE1="${i##*/}"
+#      FILE2="${j##*/}"
+#      if [ "$FILE1" = "$FILE2" ];
+#      then echo ${i} " and " ${j}; UNI=$(sdiff -B -b -s -i $i $j | wc -l); ILINES=$(cat $i | wc -l) && JLINES=$(cat $j | wc -l);
+#      ((JLINES+=1));
+#      ((ILINES+=1));
+#      #echo "UNI: $UNI";
+#      #echo "ILINES: $ILINES";
+#      #echo "JLINES: $JLINES";
+#      TOT=$[ILINES + JLINES];
+#      #echo "TOT: $TOT";
+#      SUB=$[TOT - UNI];
+#      #echo "SUB: $SUB";
+#      DIV=$[SUB / 2];
+#      #echo "DIV: $DIV";
+#      PERC=$(bc <<< "scale=2;$DIV/$JLINES");
+#      #echo "PERC: $PERC";
+#      FINAL=$(bc <<< "scale=1;$PERC*100");
+#      echo "${FINAL%.*}% the same" && echo;
+#      fi
+#   done;
+#done
